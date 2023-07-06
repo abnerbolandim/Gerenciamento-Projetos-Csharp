30cb04a4197b91fb8a2cf458138e54a4bc741327
Abner <contato.abnerbolandim@gmail.com>
2023-07-06 11:49:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GerenciamentoProjetos
+{
+    public class Project
+    {
+        public string Title{get; set;}
+        public string Description{get; set;}
+        public int IDate{get; set;}
+        public int FDate{get; set;}
+        public bool Status{get; set;}
+
+
+    }
+
+    public class Management
+    {
+        private List<Project> projectList;
+
+        public Management()
+        {
+            projectList = new List<Project>();
+        } 
+
+        public void AddProject(Project project)
+        {
+            projectList.Add(project);
+            Console.WriteLine("Projeto adicionado com sucesso!");
+        }
+    }
+
+}
